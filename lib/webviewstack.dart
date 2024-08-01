e863f523218703b28bdc98d3b2cfc34fce64f381
Bharathi <bharathi.p@fcsonline.co.in>
2024-08-01 12:15:33 +0530

    modification

--- lib/webviewstack.dart
+++ lib/webviewstack.dart
@@ -3,14 +3,13 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewStack extends StatefulWidget {
   const WebViewStack({required this.controller, super.key});
- final WebViewController controller;   
+  final WebViewController controller;
   @override
   State<WebViewStack> createState() => _WebViewStackState();
 }
 
 class _WebViewStackState extends State<WebViewStack> {
   var loadingPercentage = 0;
-  
 
   @override
   void initState() {
@@ -47,11 +46,11 @@ class _WebViewStackState extends State<WebViewStack> {
           });
         },
       ),
-      
     );
-      widget.controller.loadRequest(
-        Uri.parse('https://api.flutter.dev/flutter/rendering/CustomClipper-class.html'),
-      );
+    widget.controller.loadRequest(
+      Uri.parse(
+          'https://api.flutter.dev/flutter/rendering/CustomClipper-class.html'),
+    );
   }
 
   @override
@@ -59,7 +58,7 @@ class _WebViewStackState extends State<WebViewStack> {
     return Stack(
       children: [
         WebViewWidget(
-          controller:widget. controller,
+          controller: widget.controller,
         ),
         if (loadingPercentage < 100)
           LinearProgressIndicator(
@@ -69,3 +68,126 @@ class _WebViewStackState extends State<WebViewStack> {
     );
   }
 }
+
+
+// import 'package:flutter/material.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+// void openApp(BuildContext context, String appName, String appUrl,
+//     String playStoreUrl) async {
+//   try {
+//     if (await launchUrl(Uri.parse(appUrl))) {
+//     } else {
+//       showDialog(
+//         // ignore: use_build_context_synchronously
+//         context: context,
+//         builder: (BuildContext context) {
+//           return AlertDialog(
+//             title: const Text('App Not Installed'),
+//             content: Text('The $appName app is not installed on your device.'),
+//             actions: [
+//               TextButton(
+//                 onPressed: () {
+//                   Navigator.of(context).pop();
+//                   // Launch Play Store URL
+//                   launchUrl(Uri.parse(playStoreUrl));
+//                 },
+//                 child: const Text('Install from Play Store'),
+//               ),
+//               TextButton(
+//                 onPressed: () {
+//                   Navigator.of(context).pop();
+//                 },
+//                 child: const Text('Cancel'),
+//               ),
+//             ],
+//           );
+//         },
+//       );
+//     }
+//   } catch (e) {
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: const Text('App Not Installed'),
+//           content: Text('The $appName app is not installed on your device.'),
+//           actions: [
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.of(context).pop();
+//                 // Launch Play Store URL
+//                 launchUrl(Uri.parse(playStoreUrl));
+//               },
+//               child: const Text('Install from Play Store'),
+//             ),
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.of(context).pop();
+//               },
+//               child: const Text('Cancel'),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+// }
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:webview_flutter_android/webview_flutter_android.dart';
+// import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+// class WebViewChart extends StatefulWidget {
+//   const WebViewChart({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<WebViewChart> createState() => _WebViewChartState();
+// }
+
+// class _WebViewChartState extends State<WebViewChart>
+//     with TickerProviderStateMixin {
+//   WebViewController controller = WebViewController();
+//   void controllerUrl(WebViewController controller, BuildContext context) async {
+//     await controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+//     await controller
+//         .loadRequest(Uri.parse('https://taikonz.io/tradeview/BTC_USDT'));
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     late final PlatformWebViewControllerCreationParams params;
+//     if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+//       params = WebKitWebViewControllerCreationParams(
+//         allowsInlineMediaPlayback: true,
+//         mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+//       );
+//     } else {
+//       params = const PlatformWebViewControllerCreationParams();
+//     }
+//     controller = WebViewController.fromPlatformCreationParams(params);
+//     controllerUrl(controller, context);
+//     if (controller.platform is AndroidWebViewController) {
+//       AndroidWebViewController.enableDebugging(false);
+//       (controller.platform as AndroidWebViewController)
+//           .setMediaPlaybackRequiresUserGesture(true);
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text(widget.title),
+//         ),
+//         body: WebViewWidget(
+//           controller: controller,
+//         ),
+//       ),
+//     );
+//   }
+// }
